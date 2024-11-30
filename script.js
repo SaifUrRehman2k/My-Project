2d3f2425643365fe28038475a6a09537314f78d9
SaifUrRehman2k <surgamer2007@gmail.com>
2024-11-30 15:39:18 +0500

    alot of changes
    2 new pages shop page and a blog page
    updated about page to story page and many  other changes to sidebar as well

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let sidebar = document.getElementById("sidebar")
 function sideBarOpen() {
-    sidebar.style.right = "0%";
+    sidebar.style.right = "0px";
 }
 function sidebarClose() {
     sidebar.style.right = "-400%";
 }
+
+let aboutNavBtn = document.getElementById("aboutNavToggler");
+let aboutArrow = document.getElementById("aboutArrow");
+let aboutNav = document.getElementById("aboutNav");
+let hideBtn = document.getElementById("hideAboutNav");
+
+aboutNavBtn.addEventListener("click", () => {
+    let showAboutBtn = document.getElementById("showAboutNav");
+    let hideAboutNav = document.getElementById("hideAboutNav");
+    showAboutBtn.classList.toggle("hideBtn");
+    hideAboutNav.classList.toggle("hideBtn");
+    aboutArrow.classList.toggle("rotate180");
+    aboutNav.classList.toggle("hide950");
+});
